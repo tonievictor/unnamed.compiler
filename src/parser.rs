d4f004114ceec7ef0e53f23ec7714b00058b4181
Tonie Victor <victornwanochi@gmail.com>
2024-12-30 07:16:10 +0100

    implemented the parser

---
+++ src/parser.rs
@@ -0,0 +1,55 @@
+use crate::lexer::{Token, TokenType, KeywordType};
+use crate::ast::{Expression, Statement, Program, FunctionDefinition};
+use std::process::exit;
+
+pub fn parse(tokens: Vec<Token>) -> Program {
+    let mut index: usize = 0;  
+    let function = parse_function(&tokens, &mut index); 
+    let program = Program{function: function};
+    return program;
+}
+
+fn parse_function(tokens: &Vec<Token>, index: &mut usize) -> FunctionDefinition {
+    // keyword type should have another enum that represent types?
+    expect(TokenType::Keyword(KeywordType::Int), tokens, index);
+
+    let name = expect(TokenType::Identifier, tokens, index).token_value;
+    expect(TokenType::OParen, tokens, index);
+    // implement arguments
+    expect(TokenType::Identifier, tokens, index);
+    expect(TokenType::CParen, tokens, index);
+    expect(TokenType::OBrace, tokens, index);
+
+    // statement
+    let statement = parse_statement(tokens, index);
+    expect(TokenType::CBrace, tokens, index);
+
+    FunctionDefinition{name: name, body: statement}
+}
+
+fn parse_statement(tokens: &Vec<Token>, index: &mut usize) -> Statement {
+    expect(TokenType::Keyword(KeywordType::Return), tokens, index);
+    let expression = parse_expression(tokens, index);
+    expect(TokenType::SemiColon, tokens, index);
+    Statement::Return(expression)
+}
+
+fn parse_expression(tokens: &Vec<Token>, index: &mut usize) -> Expression {
+    let t = expect(TokenType::Constant, tokens, index);
+    return Expression::Constant(t.token_value.parse::<u32>().unwrap());
+}
+
+fn expect(token_type:TokenType, tokens: &Vec<Token>, index: &mut usize) -> Token {
+    match tokens.get(*index) {
+        Some(t) => {
+            if token_type == t.token_type {
+                *index += 1;
+                return t.clone();
+            } else {
+                eprintln!("Syntax error > expected {:?} found {:?}", token_type, t.token_type);
+                exit(1);
+            }
+        }
+        None => {todo!()}
+    }
+}
