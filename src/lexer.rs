af388f2d3ae74f19af780d50f4bc11da3a9a7251
Tonie Victor <victornwanochi@gmail.com>
2025-06-22 19:44:18 +0100

    string tokenization implementation

--- src/lexer.rs
+++ src/lexer.rs
@@ -33,7 +33,7 @@ pub struct Token {
     pub col_num: u32,
 }
 
-pub fn tokenize(file_content: String) -> Result<Option<Vec<Token>>, String> {
+pub fn tokenize(file_content: String) -> Result<Vec<Token>, String> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut line = 1;
     let mut col = 0;
@@ -47,25 +47,55 @@ pub fn tokenize(file_content: String) -> Result<Option<Vec<Token>>, String> {
                 continue;
             }
             Some('(') => {
-                tokens.push(create_token(TokenType::OParen, String::from('('), line, col));
+                tokens.push(create_token(
+                    TokenType::OParen,
+                    String::from('('),
+                    line,
+                    col,
+                ));
             }
             Some(')') => {
-                tokens.push(create_token(TokenType::CParen, String::from(')'), line, col));
+                tokens.push(create_token(
+                    TokenType::CParen,
+                    String::from(')'),
+                    line,
+                    col,
+                ));
             }
             Some('{') => {
-                tokens.push(create_token(TokenType::OBrace, String::from('{'), line, col));
+                tokens.push(create_token(
+                    TokenType::OBrace,
+                    String::from('{'),
+                    line,
+                    col,
+                ));
             }
             Some('}') => {
-                tokens.push(create_token(TokenType::CBrace, String::from('}'), line, col));
+                tokens.push(create_token(
+                    TokenType::CBrace,
+                    String::from('}'),
+                    line,
+                    col,
+                ));
             }
             Some('+') => {
                 tokens.push(create_token(TokenType::Plus, String::from('+'), line, col));
             }
             Some('*') => {
-                tokens.push(create_token(TokenType::Multiply, String::from('*'), line, col));
+                tokens.push(create_token(
+                    TokenType::Multiply,
+                    String::from('*'),
+                    line,
+                    col,
+                ));
             }
             Some(';') => {
-                tokens.push(create_token(TokenType::SemiColon, String::from(';'), line, col));
+                tokens.push(create_token(
+                    TokenType::SemiColon,
+                    String::from(';'),
+                    line,
+                    col,
+                ));
             }
             Some('=') => {
                 tokens.push(create_token(TokenType::Equal, String::from('='), line, col));
@@ -73,28 +103,63 @@ pub fn tokenize(file_content: String) -> Result<Option<Vec<Token>>, String> {
             Some(' ') | Some('\t') => {
                 continue;
             }
+            Some('-') => {
+                let t = match chars.peek() {
+                    Some('>') => {
+                        chars.next();
+                        let coln = col;
+                        col += 1;
+                        create_token(TokenType::Arrow, String::from("->"), line, coln)
+                    }
+                    _ => create_token(TokenType::Minus, String::from('-'), line, col),
+                };
+                tokens.push(t);
+            }
+            Some('/') => {
+                if let Some(next_char) = chars.peek() {
+                    if *next_char == '/' {
+                        while chars.next_if(|&x| x != '\n').is_some() {}
+                        continue;
+                    }
+                }
+                tokens.push(create_token(
+                    TokenType::Divide,
+                    String::from('/'),
+                    line,
+                    col,
+                ));
+            }
+            Some('"') => {
+                let i = col;
+                let mut tok = String::new();
+                loop {
+                    col += 1;
+                    match chars.next() {
+                        Some('"') => break,
+                        Some('\\') => {
+                            match chars.next() {
+                                Some('n') => {col +=1; tok.push('\n');}
+                                Some('"') => {col +=1; tok.push('"');}
+                                Some('\\') => {col +=1; tok.push('\\');}
+                                Some('t') => {col +=1; tok.push('\t');}
+                                Some(c) => {
+                                    col +=1;
+                                    tok.push('\\');
+                                    tok.push(c);
+                                }
+                                None => return Err(format!("SYNTAX ERROR {},{}: unexpected end of file found before string literal termination", line, col))
+                            }
+                        }
+                        Some('\n') => return Err(format!("SYNTAX ERROR {}, {}: multiline Strings are not supported", line, col)),
+                        Some(c) => tok.push(c),
+                        None => return Err(format!("SYNTAX ERROR {},{}: unexpected end of file found before string literal termination", line, col))
+                    }
+                }
+                tokens.push(create_token(TokenType::String, tok, line, i));
+            }
             Some(c) => {
                 let mut i = 0;
-                if c == '-' {
-                    let t = match chars.peek() {
-                        Some('>') => {
-                            chars.next();
-                            let coln = col;
-                            col += 1;
-                            create_token(TokenType::Arrow, String::from("->"), line, coln)
-                        }
-                        _ => create_token(TokenType::Minus, String::from(c), line, col),
-                    };
-                    tokens.push(t);
-                } else if c == '/' {
-                    if let Some(next_char) = chars.peek() {
-                        if *next_char == '/' {
-                            while chars.next_if(|&x| x != '\n').is_some() {}
-                            continue;
-                        }
-                    }
-                    tokens.push(create_token(TokenType::Divide, String::from(c), line, col));
-                } else if c.is_ascii_alphabetic() {
+                if c.is_ascii_alphabetic() {
                     let mut tok = String::from(c);
                     while let Some(t) =
                         chars.next_if(|&x| matches!(x, 'A'..='Z' | 'a'..='z' | '_' | '0'..='9'))
@@ -135,10 +200,7 @@ pub fn tokenize(file_content: String) -> Result<Option<Vec<Token>>, String> {
         }
     }
 
-    if tokens.is_empty() {
-        return Ok(None);
-    }
-    Ok(Some(tokens))
+    Ok(tokens)
 }
 
 fn create_keyword_or_identifier(tok: String, line: u32, col: u32) -> Token {
