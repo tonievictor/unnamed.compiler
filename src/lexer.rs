1442bb22b8a67a724f6c3dea312c808e3fbdf530
Tonie Victor <victornwanochi@gmail.com>
2025-07-16 05:30:46 +0100

    reimplement lexer: literals are now associated with their tokentype and stored as slices. in the previous implementation, I stored it as a heap-allocated string associated with the token which was inefficient for this usecase.

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,10 +6,10 @@ pub enum KeywordType {
 }
 
 #[derive(PartialEq, Clone, Debug)]
-pub enum TokenType {
-    Constant,
-    Identifier,
-    StringLiteral,
+pub enum TokenType<'a> {
+    Constant(&'a str),
+    Identifier(&'a str),
+    StringLiteral(&'a str),
     Keyword(KeywordType),
     OParen,
     CParen,
@@ -20,81 +20,50 @@ pub enum TokenType {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Token {
-    pub ttype: TokenType,
-    pub literal: String,
+pub struct Token<'a> {
+    pub ttype: TokenType<'a>,
     pub line: u32,
     pub col: u32,
 }
 
-pub fn tokenize(code: String) -> Result<Vec<Token>, String> {
+pub fn tokenize<'a>(code: &'a str) -> Result<Vec<Token<'a>>, String> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut lnum = 1;
     let mut cnum = 0;
-    let mut chars = code.chars().peekable();
+    let mut chars = code.char_indices().peekable();
     loop {
         cnum += 1;
         match chars.next() {
-            Some('\n') => {
+            Some((_, '\n')) => {
                 lnum += 1;
                 cnum = 0;
                 continue;
             }
-            Some('(') => {
-                tokens.push(create_token(
-                    TokenType::OParen,
-                    String::from("("),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, '(')) => {
+                tokens.push(create_token(TokenType::OParen, lnum, cnum));
             }
-            Some(')') => {
-                tokens.push(create_token(
-                    TokenType::CParen,
-                    String::from(")"),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, ')')) => {
+                tokens.push(create_token(TokenType::CParen, lnum, cnum));
             }
-            Some('{') => {
-                tokens.push(create_token(
-                    TokenType::OBrace,
-                    String::from("{"),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, '{')) => {
+                tokens.push(create_token(TokenType::OBrace, lnum, cnum));
             }
-            Some('}') => {
-                tokens.push(create_token(
-                    TokenType::CBrace,
-                    String::from("}"),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, '}')) => {
+                tokens.push(create_token(TokenType::CBrace, lnum, cnum));
             }
-            Some(';') => {
-                tokens.push(create_token(
-                    TokenType::Semicolon,
-                    String::from(";"),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, ';')) => {
+                tokens.push(create_token(TokenType::Semicolon, lnum, cnum));
             }
-            Some('=') => {
-                tokens.push(create_token(
-                    TokenType::Equal,
-                    String::from("="),
-                    lnum,
-                    cnum,
-                ));
+            Some((_, '=')) => {
+                tokens.push(create_token(TokenType::Equal, lnum, cnum));
             }
-            Some(' ') | Some('\t') => {
+            Some((_, ' ')) | Some((_, '\t')) => {
                 continue;
             }
-            Some('/') => {
-                if let Some(next_char) = chars.peek() {
+            Some((_, '/')) => {
+                if let Some((_, next_char)) = chars.peek() {
                     if *next_char == '/' {
-                        while chars.next_if(|&x| x != '\n').is_some() {}
+                        while chars.next_if(|&(_, x)| x != '\n').is_some() {}
                         continue;
                     }
                 }
@@ -103,61 +72,54 @@ pub fn tokenize(code: String) -> Result<Vec<Token>, String> {
                     lnum, cnum, '/'
                 ));
             }
-            Some('"') => {
+            Some((start, '"')) => {
                 let i = cnum;
-                let mut tok = String::from("\"");
-                loop {
+                let end = loop {
                     cnum += 1;
                     match chars.next() {
-                        Some('"') => {
-                            tok.push('"');
-                            break;
-                        },
-                        Some('\\') => {
-                            match chars.next() {
-                                Some(c) => {
-                                    cnum +=1;
-                                    tok.push('\\');
-                                    tok.push(c);
-                                }
-                                None => return Err(format!("SYNTAX ERROR {{{},{}}}: unexpected end of file found before string literal termination", lnum, cnum))
-                            }
-                        }
-                        Some('\n') => return Err(format!("SYNTAX ERROR {{{},{}}}: unexpected end of file before string literal termination", lnum, cnum)),
-                        Some(c) => tok.push(c),
-                        None => return Err(format!("SYNTAX ERROR {{{},{}}}: unexpected end of file found before string literal termination", lnum, cnum))
+                        Some((e, '"')) => break e,
+                        Some((_, '\n')) => return Err(
+                            format!("SYNTAX ERROR {lnum},{cnum}: unexpected end of file before string literal termination"
+                        )),
+                        Some(_) => continue,
+                        None => return Err(
+                            format!("SYNTAX ERROR {lnum},{cnum}: unexpected end of file found before string literal termination"
+                        ))
                     }
-                }
-                tokens.push(create_token(TokenType::StringLiteral, tok, lnum, i));
+                };
+                tokens.push(create_token(
+                    TokenType::StringLiteral(&code[start + 1..end as usize]),
+                    lnum,
+                    i,
+                ));
             }
-            Some(c) => {
-                let mut i = 0;
-                if c.is_ascii_alphabetic() {
-                    let mut tok = String::from(c);
-                    while let Some(t) =
-                        chars.next_if(|&x| matches!(x, 'A'..='Z' | 'a'..='z' | '_' | '0'..='9'))
-                    {
-                        i += 1;
-                        tok.push(t);
-                    }
-                    tokens.push(create_keyword_or_identifier(tok, lnum, cnum));
-
-                    cnum += i;
-                } else if c.is_ascii_digit() {
-                    let mut tok = String::from(c);
-                    while let Some(t) = chars.next_if(|&x| x.is_ascii_digit()) {
-                        i += 1;
-                        tok.push(t);
-                    }
-
-                    tokens.push(create_token(TokenType::Constant, tok, lnum, cnum));
-                    cnum += i;
-                } else {
-                    return Err(format!(
-                        "{}:{}: Illegal character '{}' in program",
-                        lnum, cnum, c
-                    ));
+            Some((start, 'a'..='z' | 'A'..='Z' | '_')) => {
+                let i = cnum;
+                while chars
+                    .next_if(|&(_, x)| matches!(x, 'a'..='z' | 'A'..='Z' | '_' | '0'..='9'))
+                    .is_some()
+                {
+                    cnum += 1;
                 }
+                tokens.push(create_keyword_or_identifier(
+                    &code[start..start + (cnum - i) as usize + 1],
+                    lnum,
+                    i,
+                ))
+            }
+            Some((start, '0'..='9')) => {
+                let i = cnum;
+                while chars.next_if(|&(_, x)| x.is_ascii_digit()).is_some() {
+                    cnum += 1;
+                }
+                tokens.push(create_token(
+                    TokenType::Constant(&code[start..start + (cnum - i) as usize + 1]),
+                    lnum,
+                    i,
+                ))
+            }
+            Some((_, c)) => {
+                return Err(format!("{lnum}:{cnum}: Illegal character '{c}' in program"));
             }
             None => {
                 break;
@@ -168,21 +130,20 @@ pub fn tokenize(code: String) -> Result<Vec<Token>, String> {
     Ok(tokens)
 }
 
-fn create_keyword_or_identifier(tok: String, line: u32, col: u32) -> Token {
-    let ttype = match tok.as_str() {
+fn create_keyword_or_identifier(tok: &str, line: u32, col: u32) -> Token {
+    let ttype = match tok {
         "fn" => TokenType::Keyword(KeywordType::Fn),
         "let" => TokenType::Keyword(KeywordType::Let),
         "return" => TokenType::Keyword(KeywordType::Return),
-        _ => TokenType::Identifier,
+        _ => TokenType::Identifier(tok),
     };
 
-    create_token(ttype, tok, line, col)
+    create_token(ttype, line, col)
 }
 
-fn create_token(tok_type: TokenType, literal: String, line: u32, col: u32) -> Token {
+fn create_token(tok_type: TokenType, line: u32, col: u32) -> Token {
     Token {
         ttype: tok_type,
-        literal,
         line,
         col,
     }
--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,15 @@ use std::fs;
 use std::process::exit;
 
 pub mod lexer;
-pub mod parser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 || args.len() > 3 {
-        println!("Usage {0} <OPTIONS>", &args[0]);
+    if args.len() != 2 {
+        println!("Usage {0} <filename>", &args[0]);
         exit(1);
     }
 
-    let mut option = "";
-
-    if args.len() > 2 {
-        match args[1].as_str() {
-            "--lex" | "--codegen" | "--parse" => {
-                option = args[1].as_str();
-            }
-            _ => {
-                eprintln!("Invalid mode");
-                exit(1);
-            }
-        }
-    }
-
-    let filename = if args.len() > 2 { &args[2] } else { &args[1] };
-    // if &filename[(filename.len() - 2)..] != ".c" {
-    //     eprintln!("Cannot execute {}: Invalid filetype", filename);
-    //     exit(1);
-    // }
-
-    let file_content = match fs::read_to_string(filename) {
+    let file_content = match fs::read_to_string(&args[1]) {
         Ok(f) => f,
         Err(err) => {
             eprintln!("ERROR {}: {}", &args[1], err);
@@ -40,7 +19,7 @@ fn main() {
         }
     };
 
-    let tokens = match lexer::tokenize(file_content) {
+    let tokens = match lexer::tokenize(&file_content) {
         Ok(t) => t,
         Err(err) => {
             eprintln!("ERROR {}: {}", &args[1], err);
@@ -48,8 +27,5 @@ fn main() {
         }
     };
 
-    if option == "--lex" {
-        exit(0);
-    }
-    parser::parse(&mut tokens.iter());
+    println!("{tokens:#?}")
 }
